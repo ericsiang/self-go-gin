82880f2f982687ec73a44314e908fb73542d092e
eric <chang.eric@degames.com.tw>
2023-12-07 16:22:57 +0800

    Fix zapLogger_test

--- test/zapLoggger_test.go
+++ test/zapLoggger_test.go
@@ -7,12 +7,14 @@ import (
 )
 
 func TestNewLogger(t *testing.T) {
+	// MaxAge and RotationCount cannot be both set  兩者不能同時設置
+	// 官方 github 上有說明建議使用 WithRotationCount，要將 MaxAge 設為 -1 比較保險
 	config := &zapLogger.RotatelogsConfig{
 		InfoLogPath:   "./info.log",
 		ErrorLogPath:  "./error.log",
 		MaxSize:       1024,
 		RotationCount: 7,
-		MaxAge:        24 * time.Hour,
+		MaxAge:        -1,
 		RotationTime:  24 * time.Hour,
 	}
 
