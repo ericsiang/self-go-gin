4c1806a9c59d1f2f1cb8fe16d7a729ae95c4eddb
github-ericsiang <ericchang60917@gmail.com>
2024-06-17 14:11:12 +0800

    新增  viper 動態監聽 env file

--- initialize/env.go
+++ initialize/env.go
@@ -1,17 +1,21 @@
 package initialize
 
 import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
-
+var envPath = "env.yaml"
 
 func initEnv(serverConfig *ServerConfig) {
 	//viper 讀取環境變量的套件
 	v := viper.New()
 
 	//讀取配置文件
-	v.SetConfigFile("env.yaml")
+	v.SetConfigFile(envPath)
+
 	//讀取配置信息
 	if err := v.ReadInConfig(); err != nil {
 		panic(err)
@@ -19,4 +23,8 @@ func initEnv(serverConfig *ServerConfig) {
 	if err := v.Unmarshal(&serverConfig); err != nil {
 		panic(err)
 	}
+	v.WatchConfig() // 監聽 env file
+	v.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
 }
